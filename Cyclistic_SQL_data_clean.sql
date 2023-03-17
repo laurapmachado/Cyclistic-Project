cec83a7fd70191a0e159f9e9d1b8b852df182415
Laura Machado <124676640+laurapmachado@users.noreply.github.com>
2023-03-17 18:35:08 +0000

    Add files via upload

---
+++ Cyclistic_SQL_data_clean.sql
@@ -0,0 +1,78 @@
+--Unite all month tables into one year table
+
+SELECT * INTO trips_2022
+FROM (
+	SELECT * FROM PortfolioProject..tripdata_2022_01
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_02
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_03
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_04
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_05
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_06
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_07
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_08
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_09
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_10
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_11
+	UNION 
+	SELECT * FROM PortfolioProject..tripdata_2022_12 )a
+
+
+--check for data types
+
+SELECT COLUMN_NAME, DATA_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE 
+     TABLE_NAME   = 'tripdata_2022_09'
+
+--change data types
+
+ALTER TABLE tripdata_2022_04
+ALTER COLUMN start_station_id nvarchar(4000);
+
+ALTER TABLE tripdata_2022_07
+ALTER COLUMN start_station_id nvarchar(4000);
+
+ALTER TABLE tripdata_2022_10
+ALTER COLUMN start_station_id nvarchar(4000);
+
+ALTER TABLE tripdata_2022_11
+ALTER COLUMN start_station_id nvarchar(4000);
+
+ALTER TABLE tripdata_2022_08
+ALTER COLUMN end_station_id nvarchar(4000);
+
+ALTER TABLE tripdata_2022_09
+ALTER COLUMN end_station_id nvarchar(4000);
+
+ALTER TABLE tripdata_2022_11
+ALTER COLUMN end_station_id nvarchar(4000);
+
+ALTER TABLE tripdata_2022_12
+ALTER COLUMN end_station_id nvarchar(4000);
+
+
+--order table by start date
+
+SELECT * FROM PortfolioProject..trips_2022
+ORDER BY started_at
+
+--check for inconsistencies in tha data
+
+SELECT DISTINCT rideable_type
+FROM PortfolioProject..trips_2022
+
+SELECT DISTINCT member_casual
+FROM PortfolioProject..trips_2022
+
+
+
